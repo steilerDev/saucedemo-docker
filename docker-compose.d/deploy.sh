92ee09460755a0c82113b7d1b397b1d77969c5ae
Frank Steiler <frank@steilerdev.de>
2022-03-20 14:03:43 +0000

    Cleaning up

--- docker-compose.d/deploy.sh
+++ docker-compose.d/deploy.sh
@@ -2,7 +2,9 @@
 
 HOST="https://saucedemo.steilergroup.net"
 DEFAULT_YML="./default.yml"
+DEFAULT_CMD="default"
 DIFF_YML="./diff.yml"
+DIFF_CMD="diff"
 
 echo -n "Stopping current deployment ..."
 docker-compose -f $DEFAULT_YML down -t 0 > /dev/null 2>&1
@@ -12,14 +14,15 @@ if [ "$1" = "pull" ]; then
     echo "Updating container image..."
     docker-compose -f $DEFAULT_YML pull
 else
-    if [ "$1" = "default" ]; then
+    if [ "$1" = "$DEFAULT_CMD" ] || [ -z $1 ]; then
         echo "Bringing default site up ..."
         docker-compose -f $DEFAULT_YML up -d > /dev/null
-    elif [ "$1" = "diff" ]; then
+    elif [ "$1" = "$DIFF_CMD" ]; then
         echo "Bringing diff site up ..."
         docker-compose -f $DIFF_YML up -d > /dev/null
     else
-        echo "No argument specified, either 'default' or 'diff'"
+        echo "Unknown argument specified ($1), either '$DEFAULT_CMD' or '$DIFF_CMD'"
+        exit 1
     fi
 
     echo -n "Waiting for site to deploy ..."
