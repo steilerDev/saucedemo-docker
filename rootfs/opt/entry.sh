86f48793c6cf4c00e4ad3cd155b930a9bb3df353
Frank Steiler <frank@steilerdev.de>
2022-03-20 13:07:17 +0000

    Cleanup

--- rootfs/opt/entry.sh
+++ rootfs/opt/entry.sh
@@ -4,19 +4,24 @@ handle-stop () {
     echo "Received stop signal..."
     kill -9 $$
 }
+trap handle-stop SIGTERM
 
-echo "Starting Sauce Demo!"
+VANILLA_BRANCH_NAME="default"
+MOD_BRANCH_NAME="diff"
 
 BUILD_VANILLA="/opt/vanilla"
 BUILD_MOD="/opt/mod"
 
+echo "Starting Sauce Demo on port ${PORT:=3000}, using branch ${BRANCH:=$VANILLA_BRANCH_NAME}"
+echo $PORT
+echo $BRANCH
 
-if [ "$BRANCH" = "default" ] || [ -z $BRANCH ]; then
+if [ "$BRANCH" = "$VANILLA_BRANCH_NAME" ]; then
     echo "Starting Sauce Demo in Default Mode!"
-    serve -n -u -d --no-etag $BUILD_VANILLA
-elif [ "$BRANCH" = "diff" ]; then
+    serve -n -u -d --no-etag -l $PORT $BUILD_VANILLA
+elif [ "$BRANCH" = "$MOD_BRANCH_NAME" ]; then
     echo "Starting Sauce Demo in Diff Mode!"
-    serve -n -u -d --no-etag $BUILD_MOD
+    serve -n -u -d --no-etag -l $PORT $BUILD_MOD
 else
     echo "Unknown branch ($BRANCH) specified, aborting..."
     exit
